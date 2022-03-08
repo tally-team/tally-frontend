a978b33de3ed9d8e7c8e62411aaec1544e4f21a0
Cindy Lee <leecindy405@gmail.com>
2022-03-07 22:31:14 -0500

    attempt to resolve test issues

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    "preset": "jest-expo",
     "collectCoverage": true,
     "rootDir": "./",
     "testRegex": "tests/.+\\.test\\.js",
@@ -10,7 +11,7 @@ module.exports = {
       "node_modules",
       "src"
     ],
-    transformIgnorePatterns: [
-        "/node_modules/(?!(@react-native|react-native)/).*/"
+    "transformIgnorePatterns": [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
     ]
 }
--- src/tests/components/Tip.test.js
+++ src/tests/components/Tip.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+// import { render, fireEvent } from '@testing-library/react-native';
 import Tip from '../../components/Tip';
 
 describe('Tip component', () => {
