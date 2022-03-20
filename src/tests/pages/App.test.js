d0430eb32683041941a537bab2ad77334f183fab
Cindy Lee <leecindy405@gmail.com>
2022-03-20 09:14:46 -0400

    testing app test

---
+++ src/tests/pages/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../App';
+import AddItem from '../../components/AddItem';
+import Tip from '../../components/Tip';
+
+describe('App page', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('loads all the expected components', () => {
+    expect(wrapper.find(AddItem)).toHaveLength(1);
+    expect(wrapper.find(Tip)).toHaveLength(1);
+  });
+});
