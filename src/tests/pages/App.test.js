448efd2b8d5d03cf10ffac948cf80ab3f5482925
Cindy Lee <leecindy405@gmail.com>
2022-03-27 11:56:15 -0400

    adding check for add party member component in app test

--- src/tests/pages/App.test.js
+++ src/tests/pages/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../App';
 import AddItem from '../../components/AddItem';
+import AddPartyMember from '../../components/AddPartyMember';
 import Tip from '../../components/Tip';
 
 describe('App page', () => {
@@ -17,6 +18,7 @@ describe('App page', () => {
 
   it('loads all the expected components', () => {
     expect(wrapper.find(AddItem)).toHaveLength(1);
+    expect(wrapper.find(AddPartyMember)).toHaveLength(1);
     expect(wrapper.find(Tip)).toHaveLength(1);
   });
 });
