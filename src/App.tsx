c2b44692df9b99f4edc30510d031e05f5f88bc24
Cindy Lee <leecindy405@gmail.com>
2023-07-09 15:36:51 -0400

    refactor app to use usereducer instead of usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,119 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddItem from './components/AddItem';
 import AddPartyMember from './components/AddPartyMember';
 import Tax from './components/Tax';
 import Tip from './components/Tip';
 
-export default function App() {
-  const [total, setTotal] = useState(0);
-  const [items, setItems] = useState({});
-  const [party, setParty] = useState([]);
-  const [tip, setTip] = useState(0);
-  const [tax, setTax] = useState(0);
+enum Actions {
+  ADD_ITEM = 'add_item',
+  ADD_PARTY_MEMBER = 'add_party_member',
+  SET_TIP = 'set_tip',
+  SET_TAX = 'set_tax',
+}
 
-  const addItem = (itemName: string, itemPrice: number, itemPurchaserList: Array<string>) => {
-    setItems({ ...items, [itemName]: [itemPrice, itemPurchaserList] });
-    setTotal(Number((total + itemPrice).toFixed(2)));
+type AppStateType = {
+  total: number;
+  items: {
+    [name: string]: {
+      price: number;
+      purchaserList: string[];
+    };
+  };
+  party: string[];
+  tip: number;
+  tax: number;
+};
+
+type ActionArgs = {
+  type: Actions;
+
+  // item name or party member name
+  name: string;
+
+  // item details
+  price: number;
+  purchaserList: string[];
+
+  // tax or tip amount
+  amount: number;
+};
+
+export default function App() {
+  const initialState: AppStateType = {
+    total: 0,
+    items: {},
+    party: [],
+    tip: 0,
+    tax: 0,
   };
 
-  const addPartyMember = (memberName: string) => {
-    setParty([...party, memberName]);
+  const reducer = (state: AppStateType, action: Partial<ActionArgs>) => {
+    switch (action.type) {
+      case Actions.ADD_ITEM: {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [action.name]: {
+              price: action.price,
+              purchaserList: action.purchaserList,
+            },
+          },
+          total: Number((state.total + action.price).toFixed(2)),
+        };
+      }
+      case Actions.ADD_PARTY_MEMBER: {
+        return {
+          ...state,
+          party: [...state.party, action.name],
+        };
+      }
+      case Actions.SET_TIP: {
+        return {
+          ...state,
+          tip: action.amount,
+        };
+      }
+      case Actions.SET_TAX: {
+        return {
+          ...state,
+          tax: action.amount,
+        };
+      }
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addItem = (name: string, price: number, purchaserList: Array<string>) => {
+    dispatch({
+      type: Actions.ADD_ITEM,
+      name: name,
+      price: price,
+      purchaserList: purchaserList,
+    });
+  };
+
+  const addPartyMember = (name: string) => {
+    dispatch({
+      type: Actions.ADD_PARTY_MEMBER,
+      name: name,
+    });
+  };
+
+  const setTip = (amount: number) => {
+    dispatch({
+      type: Actions.SET_TIP,
+      amount: amount,
+    });
+  };
+
+  const setTax = (amount: number) => {
+    dispatch({
+      type: Actions.SET_TAX,
+      amount: amount,
+    });
   };
 
   return (
@@ -27,7 +122,7 @@ export default function App() {
         <>
           <Text>Party</Text>
 
-          {party.map((name, index) => (
+          {state.party.map((name, index) => (
             <Text key={`${name}-${index}`}>{name}</Text>
           ))}
           <Text>
@@ -40,12 +135,12 @@ export default function App() {
             {`\n`}
           </Text>
           <Text>Item List</Text>
-          {Object.keys(items).map((itemName) => {
-            const [itemPrice, purchaserList] = [...items[itemName]];
+          {Object.keys(state.items).map((itemName) => {
+            const { price, purchaserList } = state.items[itemName];
             return (
               <View style={styles.row} key={itemName}>
                 <Text>
-                  {itemName}: {itemPrice}
+                  {itemName}: {price}
                 </Text>
                 <Text>Purchasers: {purchaserList.join(', ')}</Text>
               </View>
@@ -55,13 +150,13 @@ export default function App() {
             {`\n`}
             {`\n`}
           </Text>
-          <AddItem addItem={addItem} party={party} />
-          <Tax tax={tax} setTax={setTax} />
-          <Text>Tax: {tax.toFixed(2)}</Text>
-          <Tip total={total} setTip={setTip} />
-          <Text>Tip: {tip.toFixed(2)}</Text>
+          <AddItem addItem={addItem} party={state.party} />
+          <Tax tax={state.tax} setTax={setTax} />
+          <Text>Tax: {state.tax.toFixed(2)}</Text>
+          <Tip total={state.total} setTip={setTip} />
+          <Text>Tip: {state.tip.toFixed(2)}</Text>
         </>
-        <Text>Subtotal: {total}</Text>
+        <Text>Subtotal: {state.total}</Text>
       </>
     </View>
   );
