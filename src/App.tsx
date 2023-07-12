c28399a98493df2dcd3cc54e86930df5f78f7969
Cindy Lee <leecindy405@gmail.com>
2023-07-11 23:26:17 -0400

    [Task-9] Refactor FE management state to use `useReducer`  (#42)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,149 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddItem from './components/AddItem';
 import AddPartyMember from './components/AddPartyMember';
 import Tax from './components/Tax';
 import Tip from './components/Tip';
 
-export default function App() {
-  const [total, setTotal] = useState(0);
-  const [items, setItems] = useState({});
-  const [party, setParty] = useState([]);
-  const [tip, setTip] = useState(0);
-  const [tax, setTax] = useState(0);
+enum Actions {
+  ADD_ITEM = 'add_item',
+  ADD_PARTY_MEMBER = 'add_party_member',
+  SET_TIP = 'set_tip',
+  SET_TAX = 'set_tax',
+}
 
-  const addItem = (itemName: string, itemPrice: number, itemPurchaserList: Array<string>) => {
-    setItems({ ...items, [itemName]: [itemPrice, itemPurchaserList] });
-    setTotal(Number((total + itemPrice).toFixed(2)));
+type AppState = {
+  total: number;
+  items: {
+    [name: string]: {
+      price: number;
+      purchaserList: string[];
+    };
+  };
+  party: string[];
+  tip: number;
+  tax: number;
+};
+
+type PartyMemberDetails = {
+  name: string;
+};
+
+type ItemDetails = {
+  name: string;
+  price: number;
+  purchaserList: string[];
+};
+
+type TaxDetails = {
+  amount: number;
+};
+
+type TipDetails = {
+  amount: number;
+};
+
+type ActionArgs = {
+  type: Actions;
+  partyMemberDetails: PartyMemberDetails;
+  itemDetails: ItemDetails;
+  taxDetails: TaxDetails;
+  tipDetails: TipDetails;
+};
+
+export default function App() {
+  const initialState: AppState = {
+    total: 0,
+    items: {},
+    party: [],
+    tip: 0,
+    tax: 0,
   };
 
-  const addPartyMember = (memberName: string) => {
-    setParty([...party, memberName]);
+  const reducer = (state: AppState, action: Partial<ActionArgs>) => {
+    switch (action.type) {
+      case Actions.ADD_ITEM: {
+        const { name, price, purchaserList } = action.itemDetails;
+        const updatedItems = {
+          ...state.items,
+          [name]: {
+            price: price,
+            purchaserList: purchaserList,
+          },
+        };
+        const total = Object.values(updatedItems).reduce(
+          (sum, itemDetail) => sum + itemDetail.price,
+          0
+        );
+
+        return {
+          ...state,
+          items: updatedItems,
+          total: Number(total.toFixed(2)),
+        };
+      }
+      case Actions.ADD_PARTY_MEMBER: {
+        const { name } = action.partyMemberDetails;
+        return {
+          ...state,
+          party: [...state.party, name],
+        };
+      }
+      case Actions.SET_TIP: {
+        const { amount } = action.tipDetails;
+        return {
+          ...state,
+          tip: amount,
+        };
+      }
+      case Actions.SET_TAX: {
+        const { amount } = action.taxDetails;
+        return {
+          ...state,
+          tax: amount,
+        };
+      }
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addItem = (name: string, price: number, purchaserList: Array<string>) => {
+    dispatch({
+      type: Actions.ADD_ITEM,
+      itemDetails: {
+        name: name,
+        price: price,
+        purchaserList: purchaserList,
+      },
+    });
+  };
+
+  const addPartyMember = (name: string) => {
+    dispatch({
+      type: Actions.ADD_PARTY_MEMBER,
+      partyMemberDetails: {
+        name: name,
+      },
+    });
+  };
+
+  const setTip = (amount: number) => {
+    dispatch({
+      type: Actions.SET_TIP,
+      tipDetails: {
+        amount: amount,
+      },
+    });
+  };
+
+  const setTax = (amount: number) => {
+    dispatch({
+      type: Actions.SET_TAX,
+      taxDetails: {
+        amount: amount,
+      },
+    });
   };
 
   return (
@@ -27,7 +152,7 @@ export default function App() {
         <>
           <Text>Party</Text>
 
-          {party.map((name, index) => (
+          {state.party.map((name, index) => (
             <Text key={`${name}-${index}`}>{name}</Text>
           ))}
           <Text>
@@ -40,12 +165,12 @@ export default function App() {
             {`\n`}
           </Text>
           <Text>Item List</Text>
-          {Object.keys(items).map((itemName) => {
-            const [itemPrice, purchaserList] = [...items[itemName]];
+          {Object.keys(state.items).map((itemName) => {
+            const { price, purchaserList } = state.items[itemName];
             return (
               <View style={styles.row} key={itemName}>
                 <Text>
-                  {itemName}: {itemPrice}
+                  {itemName}: {price}
                 </Text>
                 <Text>Purchasers: {purchaserList.join(', ')}</Text>
               </View>
@@ -55,13 +180,13 @@ export default function App() {
             {`\n`}
             {`\n`}
           </Text>
-          <AddItem addItem={addItem} party={party} />
-          <Tax tax={tax} setTax={setTax} />
-          <Text>Tax: {tax.toFixed(2)}</Text>
-          <Tip total={total} setTip={setTip} />
-          <Text>Tip: {tip.toFixed(2)}</Text>
+          <AddItem addItem={addItem} party={state.party} />
+          <Tax tax={state.tax} setTax={setTax} />
+          <Text>Tax: {state.tax.toFixed(2)}</Text>
+          <Tip total={state.total} setTip={setTip} />
+          <Text>Tip: {state.tip.toFixed(2)}</Text>
         </>
-        <Text>Subtotal: {total}</Text>
+        <Text>Subtotal: {state.total}</Text>
       </>
     </View>
   );
--- src/tests/pages/App.test.tsx
+++ src/tests/pages/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import App from '../../App';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent, within } from '@testing-library/react-native';
+import { FlatList } from 'react-native';
+import '@testing-library/jest-native';
 
 describe('App page', () => {
   let wrapper;
@@ -13,14 +15,69 @@ describe('App page', () => {
     jest.clearAllMocks();
   });
 
+  const setUpParty = () => {
+    const partyMemberNameInput = wrapper.getByPlaceholderText('Name');
+    const addPartyMemberButton = wrapper.getByText('Add Party Member');
+
+    fireEvent.changeText(partyMemberNameInput, 'Person A');
+    fireEvent.press(addPartyMemberButton);
+
+    fireEvent.changeText(partyMemberNameInput, 'Person B');
+    fireEvent.press(addPartyMemberButton);
+  };
+
+  const setUpItems = (itemsList) => {
+    const itemNameInput = wrapper.getByPlaceholderText('Item Name');
+    const priceInput = wrapper.getByPlaceholderText('0.00');
+    const addItemButton = wrapper.getByText('Add Item');
+    const partyMemberList = wrapper.root.findByType(FlatList);
+
+    itemsList.forEach((itemDetails) => {
+      const [name, price, purchaserList] = itemDetails;
+
+      fireEvent.changeText(priceInput, price);
+      fireEvent.changeText(itemNameInput, name);
+
+      purchaserList.forEach((purchaser) => {
+        const partyMember = within(partyMemberList).getByText(purchaser);
+        fireEvent.press(partyMember);
+      });
+
+      fireEvent.press(addItemButton);
+    });
+  };
+
   it('loads all the expected components', () => {
     // adding party member shows
-    expect(wrapper.queryByText('Add Party Member')).not.toBeNull();
+    expect(wrapper.queryByText('Add Party Member')).toBeOnTheScreen();
 
     // adding party item shows
-    expect(wrapper.queryByText('Add Item')).not.toBeNull();
+    expect(wrapper.queryByText('Add Item')).toBeOnTheScreen();
 
     // tip section shows
-    expect(wrapper.queryByText('15%')).not.toBeNull();
+    expect(wrapper.queryByText('15%')).toBeOnTheScreen();
+  });
+
+  it('shows correct subtotal', () => {
+    setUpParty();
+
+    const itemsList = [
+      ['pokeball', '20.50', ['Person A']],
+      ['riceball', '13.24', ['Person B']],
+    ];
+    setUpItems(itemsList);
+    expect(wrapper.getByText(/Subtotal/)).toHaveTextContent('33.74');
+  });
+
+  it('shows correct subtotal after repricing an item', () => {
+    setUpParty();
+
+    const itemsList = [
+      ['pokeball', '20.50', ['Person A']],
+      ['riceball', '13.24', ['Person B']],
+      ['pokeball', '21.53', ['Person A']],
+    ];
+    setUpItems(itemsList);
+    expect(wrapper.queryByText(/Subtotal/)).toHaveTextContent('34.77');
   });
 });
