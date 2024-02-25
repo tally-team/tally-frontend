f36b503b80d3459d1eb2568bc03152bd86ca55f0
Cindy Lee <leecindy405@gmail.com>
2024-02-25 18:40:51 -0500

    call api to submit transaction on submit click

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,11 @@
-export enum DispatchActions {
+enum DispatchActions {
   ADD_ITEM = 'add_item',
   ADD_PARTY_MEMBER = 'add_party_member',
   SET_TIP = 'set_tip',
   SET_TAX = 'set_tax',
+  RESET = 'reset'
+}
+
+export default { 
+  DispatchActions
 }
